614d978bd0ee574b584af715cd2cd83a3ade425f
Gupta <Visheshkumar.Gupta@msci.com>
2022-01-18 10:37:35 +0530

    Corrected MajorityElement2 Program

--- src/main/java/ArraysAndStrings/MajorityElement2.java
+++ src/main/java/ArraysAndStrings/MajorityElement2.java
@@ -26,7 +26,7 @@ public class MajorityElement2 {
         int value2=arr[0];
         int count2=0;
         List<Integer> output=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++){
             if(arr[i] == value1){
                 count1++;
             }
