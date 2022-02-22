c076012571b4cb30945b60ee9fdbdbdc3f844354
Gupta <Visheshkumar.Gupta@msci.com>
2022-02-22 13:19:22 +0530

    Added code for Max Chunks

---
+++ src/main/java/ArraysAndStrings/MaxChunksToMakeArraySorted.java
@@ -0,0 +1,40 @@
+package ArraysAndStrings;
+
+/*
+* Question - https://nados.io/question/max-chunks-to-make-array-sorted
+* Solution - https://www.youtube.com/watch?v=XZueXHOhO5E&list=PL-Jc9J83PIiE-TR27GB7V5TBLQRT5RnSl&index=13
+*
+* */
+
+import org.testng.annotations.Test;
+
+public class MaxChunksToMakeArraySorted {
+
+    @Test
+    public void maxChunks(){
+        int [] arr={2,1,3,0,5,5};
+        //Expected 2 chunks
+        int max=0;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max){
+                max=arr[i];
+            }
+            if(max==i){
+                count++;
+            }
+        }
+        System.out.println("Max chunks that can be formed are : " + count);
+    }
+    /*
+    * Time Complexity : O(n)
+
+      Overall time complexity of the function will be O(n) because we are using only one loop which is running from 0 to n - 1.
+
+
+      Space Complexity : O(1)
+
+      Since, no extra spaces are used to store numbers, therefore the space complexity is constant.
+    *
+    * */
+}
