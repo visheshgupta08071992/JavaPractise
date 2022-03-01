c3337db55811a12fe59259657c728d59faaaa077
Gupta <Visheshkumar.Gupta@msci.com>
2022-03-01 12:47:37 +0530

    Added program to add a no to array

---
+++ src/main/java/ArraysAndStrings/AddToArrayFormOfInteger.java
@@ -0,0 +1,50 @@
+package ArraysAndStrings;
+
+/*
+* Question - https://leetcode.com/problems/add-to-array-form-of-integer/
+*
+*
+* */
+
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddToArrayFormOfInteger {
+
+    @Test
+    public void addToArrayFormOfInteger(){
+        int [] num={2,1,5};
+        int k=806;
+        // Expected [1,0,2,1]
+        String strNum= Integer.toString(k);
+        int [] result = new int[num.length > strNum.length() ? num.length:strNum.length()];
+        List<Integer> sum = new ArrayList<>();
+        int carry=0;
+        int i=num.length-1;
+        int j=strNum.length() - 1;
+        int m=result.length -1;
+        while(m>=0){
+            int d=carry;
+            if(i>=0){
+                d= d + num[i];
+            }
+            if(j>=0){
+                d = d + (strNum.charAt(j) - '0');
+            }
+            carry=d/10;
+            result[m]=d%10;
+            i--;
+            j--;
+            m--;
+        }
+        if(carry > 0){
+            sum.add(carry);
+        }
+        for(int val : result){
+            sum.add(val);
+        }
+        System.out.println(sum);
+    }
+}
