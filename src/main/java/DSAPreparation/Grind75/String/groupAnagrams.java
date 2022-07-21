083338a63e931d4d704339820e845c4ba9d04091
Gupta <Visheshkumar.Gupta@msci.com>
2022-07-21 18:35:45 +0530

    Added program to find all anagrams of string

--- src/main/java/DSAPreparation/Grind75/String/groupAnagrams.java
+++ src/main/java/DSAPreparation/Grind75/String/groupAnagrams.java
@@ -7,6 +7,22 @@ import java.util.*;
 /*
 * leetcode - https://leetcode.com/problems/group-anagrams/
 * pepcoding code - https://www.youtube.com/watch?v=NNBQik4phMI
+*
+* Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
 * */
 
 public class groupAnagrams {
@@ -14,8 +30,8 @@ public class groupAnagrams {
 
     @Test
     public void groupAnagramBestApproachUsingSorting(){
-       // String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        String[] strs = {"",""};
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat","eat"};
+      //  String[] strs = {"",""};
         Map<String,List<String>> map=new HashMap<>();
 
         for(String str:strs){
