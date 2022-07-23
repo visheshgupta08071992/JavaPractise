f37c992175606eea8248eceb7612c3763017a8cd
Gupta <Visheshkumar.Gupta@msci.com>
2022-07-23 17:19:55 +0530

    Added programs for sorting HashMap

---
+++ src/main/java/SortHaspMap/SortHashMapBasedOnKeysUsingTreeMap.java
@@ -0,0 +1,34 @@
+package SortHaspMap;
+
+/*
+* https://www.baeldung.com/java-hashmap-sort
+* */
+
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+/*   TreeMap sorts HashMap based on Keys
+*
+* */
+public class SortHashMapBasedOnKeysUsingTreeMap {
+
+    @Test
+    public void sortHashMapUsingTreeMap(){
+        Map<String,Integer> map =new HashMap<>();
+        map.put("Ashwini",100);
+        map.put("chetan",15);
+        map.put("babu",105);
+        map.put("dog",101);
+        System.out.println(map);
+
+        //Sorting HashMap using key with the help of TreeMap
+        Map<String,Integer> treeMap =new TreeMap<>(map);
+        System.out.println(treeMap);
+    }
+}
+
