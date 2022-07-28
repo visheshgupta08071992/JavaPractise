f5ff8a944248fd01653acf1940895b25d2bb1f20
Gupta <Visheshkumar.Gupta@msci.com>
2022-07-28 12:04:43 +0530

    Added another approach for longest common prefix

--- src/main/java/DSAPreparation/Grind75/String/LongestCommonPrefix.java
+++ src/main/java/DSAPreparation/Grind75/String/LongestCommonPrefix.java
@@ -51,4 +51,23 @@ public class LongestCommonPrefix {
         }
         System.out.println(result.toString());
     }
+
+    @Test
+    public void anotherApproachWithoutUsingSort(){
+     //   String [] strs = {"flower","flow","flight"};
+        String [] strs = {"dog","racecar","car"};
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<strs[0].length();i++){
+            char c = strs[0].charAt(i);
+            for(int j=1;j<strs.length;j++){
+                if(i >= strs[j].length() || c!=strs[j].charAt(i)){
+                    System.out.println(sb.toString());
+                    return;
+                }
+            }
+            sb.append(c);
+        }
+        System.out.println(sb.toString());
+    }
 }
