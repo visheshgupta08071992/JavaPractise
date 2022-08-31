8a95c470d5c4f77f2f76454304560195711df5d6
Gupta <Visheshkumar.Gupta@msci.com>
2022-08-31 12:34:05 +0530

    Added Java program for Gas Station

---
+++ src/main/java/DSAPreparation/Grind75/Array/GasStation.java
@@ -0,0 +1,87 @@
+package DSAPreparation.Grind75.Array;
+
+/*
+* LeetCode - https://leetcode.com/problems/gas-station/
+* Explanation - https://www.youtube.com/watch?v=HQpDS9wuzws
+*
+*There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+
+
+Example 1:
+
+Input: gas = [1,2,3,4,5], cost = [3,4,5,1,2]
+Output: 3
+Explanation:
+Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 4. Your tank = 4 - 1 + 5 = 8
+Travel to station 0. Your tank = 8 - 2 + 1 = 7
+Travel to station 1. Your tank = 7 - 3 + 2 = 6
+Travel to station 2. Your tank = 6 - 4 + 3 = 5
+Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+Therefore, return 3 as the starting index.
+Example 2:
+
+Input: gas = [2,3,4], cost = [3,4,3]
+Output: -1
+Explanation:
+You can't start at station 0 or 1, as there is not enough gas to travel to the next station.
+Let's start at station 2 and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+Travel to station 0. Your tank = 4 - 3 + 2 = 3
+Travel to station 1. Your tank = 3 - 3 + 3 = 3
+You cannot travel back to station 2, as it requires 4 unit of gas but you only have 3.
+Therefore, you can't travel around the circuit once no matter where you start.
+*
+*
+* */
+
+import org.testng.annotations.Test;
+
+public class GasStation {
+    @Test
+    public void findStartStation(){
+        int [] gas={1,2,3,4,5};
+        int [] cost={3,4,5,1,2};
+        int gasCount=0;
+        int costCount=0;
+        int currentFuel=0;
+        int start=0;
+
+        //First check that the Total Gas count is more than the cost, If not then return -1
+
+        for(int i=0;i<gas.length;i++){
+            gasCount = gasCount + gas[i];
+            costCount = costCount + cost[i];
+        }
+        if(gasCount < costCount){
+            System.out.println("-1");
+            return;
+        }
+
+        //If Gas Count is more or equal to cost count then we can definately do a circular round
+
+        /*Checking at each station that the currentFuel is more than then required cost
+        * If at any point currentFuel becomes less then zero then we caanot stat from that station,Hence we need incrmenet the start
+        * pointer
+        *
+        * */
+
+        for(int i=0;i<gas.length;i++){
+            currentFuel = currentFuel + (gas[i] - cost[i]);
+            if(currentFuel < 0){
+
+                start = i + 1;
+
+                /*We are making currentFuel as zero as we would be updating the starting point
+                Hence previous fuel balance is not required
+                * */
+                currentFuel=0;
+            }
+        }
+        System.out.println(start);
+    }
+}
