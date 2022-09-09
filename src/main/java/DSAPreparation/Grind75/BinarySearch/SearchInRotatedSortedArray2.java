22692a3da90290eb1742cc850d883438536456ef
Gupta <Visheshkumar.Gupta@msci.com>
2022-09-09 17:37:59 +0530

    Added java program to search element in rotated sorted array

---
+++ src/main/java/DSAPreparation/Grind75/BinarySearch/SearchInRotatedSortedArray2.java
@@ -0,0 +1,59 @@
+package DSAPreparation.Grind75.BinarySearch;
+
+
+import org.testng.annotations.Test;
+
+/*
+* Leetcode - https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+*
+*
+* */
+public class SearchInRotatedSortedArray2 {
+
+    @Test
+    public void search(){
+        int [] nums={2,5,6,0,0,1,2,2,2,2,2};
+        int target=0;
+
+        int end = nums.length -1;
+        int start=0;
+        int mid;
+
+        while(start<=end){
+
+            mid = (start + end)/2;
+
+            if(nums[mid] == target){
+                System.out.println("true");
+                return;
+            }
+
+            //Means left array is sorted and binary search only works on sorted array
+            if(nums[mid] > nums[start]){
+
+                if(target < nums[mid] && target >= nums[start]){
+                    end = mid - 1;
+                }
+                else{
+                    start = mid + 1;
+                }
+            }
+            //Means right array is sorted and binary search only works on sorted array
+            else if(nums[start] > nums[mid]){
+
+                if(target > nums[mid] && target <=nums[end]){
+                    start = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+
+            }
+            // else both mid and start element are duplicate and start element is not target so we do start++
+            else{
+                start++;
+            }
+        }
+        System.out.println(false);
+    }
+}
