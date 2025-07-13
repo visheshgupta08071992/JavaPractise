c68912b30bf6321537c4699352205f2e2ad5655c
Vishesh Gupta <52998083+visheshgupta08071992@users.noreply.github.com>
2025-07-13 07:45:23 +0530

    Update FindMinimumInRotatedSortedArray.java

--- src/main/java/DSAPreparation/Grind75/BinarySearch/FindMinimumInRotatedSortedArray.java
+++ src/main/java/DSAPreparation/Grind75/BinarySearch/FindMinimumInRotatedSortedArray.java
@@ -31,7 +31,7 @@ Input: nums = [11,13,15,17]
 Output: 11
 Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
 *
-*
+*First two approaches can only be used if all elements are unique, If array contains duplicate elements use approach three
 * */
 
 import org.testng.annotations.Test;
@@ -81,6 +81,43 @@ public class FindMinimumInRotatedSortedArray {
 
         }
         System.out.println("-1");;
+    }
+
+    @Test
+    public void approach2(){
+        int [] nums={4,5,6,7,0,1,2};
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        System.out.println(nums[left]);
+    }
+
+    @Test
+    public void approach3WorkWithDuplicates(){
+        int [] nums={4,5,5,1,1,2,3,3};
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else if(nums[mid] < nums[right]) {
+                right = mid;
+            }
+            else{
+                right--;
+            }
+        }
+        System.out.println(nums[left]);
     }
 
 }
